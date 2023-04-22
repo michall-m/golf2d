e2552765a8dd6a96f47ba26eb43842d6d95792b8
mmisiak <mmisiak@qualtrics.com>
2023-04-23 01:50:00 +0200

    refactor nodes to easily create new levels; add automatic lvls changing

--- Holes/hole.gd
+++ Holes/hole.gd
@@ -12,11 +12,18 @@ func _ready():
 		global_position - Vector2(hole_radius, hole_radius),
 		global_position - Vector2(hole_radius, -hole_radius)]
 
+func get_next_level() -> int:
+	return get_tree().current_scene.name.substr(4).to_int() + 1
+
+func change_scene():
+	get_tree().change_scene_to_file("res://Levels/lvl_" + str(get_next_level()) + ".tscn")
+
 func is_hit(position: Vector2):
 	var is_inside = Geometry2D.is_point_in_polygon(position, hole_area)
 	if is_inside:
 		print("hole hit")
 		emit_signal("hole_hit")
+		change_scene()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
